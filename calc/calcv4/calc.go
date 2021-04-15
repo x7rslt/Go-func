8e3cf23ad19fbbd450953f1fe886d15b9f427e29
x7rslt <1660019216@qq.com>
2021-04-16 00:20:36 +0800

    switch case for command

---
+++ calc/calcv4/calc.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func Add(x, y int) {
+	fmt.Printf("%d + %d = %d\n", x, y, x+y)
+}
+
+func Sqrt(x int) {
+	fmt.Printf("%d的平方：%d\n", x, x*x)
+}
+
+var add string
+var sqrt int
+
+func main() {
+	flag.StringVar(&add, "add", "", "-add num1,num2")
+	flag.IntVar(&sqrt, "sqrt", 7, "-sqrt num")
+	flag.Parse()
+	if len(os.Args) > 2 {
+		switch os.Args[1] {
+		case "-add":
+			value := strings.Split(add, ",")
+			x, _ := strconv.Atoi(value[0])
+			y, _ := strconv.Atoi(value[1])
+			Add(x, y)
+		case "-sqrt":
+			Sqrt(sqrt)
+		}
+	}
+}
